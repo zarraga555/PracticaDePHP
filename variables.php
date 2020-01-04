a64ee57912b59ca10cfe956f311acfec9b757633
zarraga555 <zarraga555@gmail.com>
2020-01-04 00:04:29 -0400

    variables en php

---
+++ variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Variables con PHP</title>
+</head>
+<body>
+    <?php
+    
+    $variable = "Hola"; //TIPO STRING
+    echo $variable;
+    $x = 3; //TIPO ENTERO
+    $X = 5 //TIPO ENTERO (PERO SON VARIABLES DIFERENTES NO DISTENGUE ENTRE MAYUSCULAS Y MINUSCULAS )
+    echo $x 
+    $decimales= 6.25;//TIPO DECIMAL
+    echo $decimales
+    $var = true; //TIPO BOOLEANO
+    echo $var;
+    ?>
+</body>
+</html>
