b5814b6171bce7eb11f18e6b4d196486b10c05f5
zarraga555 <zarraga555@gmail.com>
2020-01-03 23:50:37 -0400

    comentarios

---
+++ conentarios.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comentarios en PHP</title>
+</head>
+<body>
+    <?php
+    //echo "Hola Mundo";
+    #echo '<br>';
+    /*
+    echo '<center>Bienvnido al curso de Aprende a programar en
+     php desde cero (Practica)</center>'*/
+    ?>
+    
+</body>
+</html>
