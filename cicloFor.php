acffc922de7b80c8a86c9111a2900db65568afa9
Alberto Zarraga <zarraga555@gmail.com>
2020-01-10 13:27:10 -0400

    Ciclo FOR

---
+++ cicloFor.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Estructura Ciclo FOR en PHP</title>
+</head>
+<body>
+    <?php
+    
+    for($num = 1; $num <= 10; $num++){
+        echo $num."<br>";
+    }
+
+    ?>
+</body>
+</html>
