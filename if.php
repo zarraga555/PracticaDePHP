2b71cd917991e8a7ec4ab9be9a52d155f15472d1
Alberto Zarraga <zarraga555@gmail.com>
2020-01-10 00:13:27 -0400

    estructura if

--- if.php
+++ if.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Constantes con PHP</title>
+    <title>Estructura IF en PHP</title>
 </head>
 <body>
     <?php
