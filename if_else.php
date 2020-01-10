f42ae3a75de2e38605defc4e5125bc7d901e5069
Alberto Zarraga <zarraga555@gmail.com>
2020-01-10 00:46:13 -0400

    estructura if else

---
+++ if_else.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Estructura IF ELSE en PHP</title>
+</head>
+<body>
+    <?php
+    
+    $edad = 17;
+    if($edad >= 18){
+        //Instrucciones Verdaderas
+        echo "Eres mayor de edad";
+    }else{
+        //Instrucciones Falsas
+        echo "No eres mayor de edad";
+    }
+
+    ?>
+</body>
+</html>
