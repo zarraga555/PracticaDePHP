95f4160623daf12566a65ea8e33da5791c7985b3
Alberto Zarraga <zarraga555@gmail.com>
2020-02-04 00:30:51 -0400

    ciclo while

---
+++ cicloWhile.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Estructura Ciclo WHILE en PHP</title>
+</head>
+<body>
+    <?php
+    /*
+    $num = 1;
+    
+    while($sum <= 5){
+        echo $sum . "<br>";
+        $num++;
+    }
+    echo "<br>";
+    */
+    echo "Taba de multiplar";
+    $n = 1;
+    $tabla = 4;
+    while($n <= 10){
+        echo $tabla. "*" .$n. "=" .$tabla * $n."<br>";
+        $n++;
+    }
+
+    ?>
+</body>
+</html>
